439b958edffe16d52ca44dfbc621d5183c17f078
ZiGoomul <lei536@gmail.com>
2017-07-15 15:07:33 +0300

    урок 04.07.2017

---
+++ Learn_JS/js/main.js
@@ -0,0 +1,32 @@
+'use strict';
+// alert("helo");
+var CONSTANT = 45;
+// number
+var num = 45646;
+var d = 4322.65;
+
+// string
+var string = "Строки";
+
+// boolean
+var t = true;
+var f = false;
+
+// null
+var n = null;
+
+// undefined
+var u;
+
+// object
+
+var obj = {asd: 'qwwee'}
+
+/////////////////////////
+
+var s = -45;
+
+/////////////////////////
+
+var f = prompt("Чето, xtnj"," bhj");
+alert(f);
